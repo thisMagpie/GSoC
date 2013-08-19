e7c63dc58b9b96adf4144a7a67000665d5b86bd0
thisMagpie <thisMagpie@users.noreply.github.com>
2013-08-19 23:00:32 +0100

    fixed a problem of overusing yet not declaring constants

--- javascript/focusCaretTracker.js
+++ javascript/focusCaretTracker.js
@@ -23,61 +23,60 @@ const Atspi = imports.gi.Atspi;
 const Lang = imports.lang;
 const Signals = imports.signals;
 
+const CARETMOVE 		= 'object:text-caret-moved';
+const STATECHANGE 		= 'object:state-changed';
+
 const FocusCaretTracker = new Lang.Class({
     Name: 'FocusCaretTracker',
 
     _init : function() {
         Atspi.init(); // TODO put somewhere better later
         this._atspiListener = Atspi.EventListener.new(Lang.bind(this, this._notifyUpdate));
-        let _stateChanged = 'object:state-changed:';
-        let _caretMoved = 'object:text-caret-moved';
+
     },
 
     // Note that select events have been included in the logic for focus events
     // only because objects will lose focus the moment they are selected.
     registerFocusListener : function() {
-        return this._atspiListener.register(this._stateChanged + 'focused') || this._atspiListener.register(
-                                                                               this._stateChanged + 'selected');
+        return this._atspiListener.register(STATECHANGE + ':focused') || this._atspiListener.register(
+                                                                               STATECHANGE + 'selected');
     },
 
     registerCaretListener : function() {
-        return this._atspiListener.register(this._caretMoved);
+        return this._atspiListener.register(this.caretMoved);
     },
 
     // Note that select events have been included in the logic for focus events
     // only because objects will lose focus the moment they are selected.
     deregisterFocusListener : function() {
-        return this._atspiListener.register(
-                        this._stateChanged + 'focused') &&
-                this._atspiListener.register(
-                        this._stateChanged + 'selected');
+        return this._atspiListener.deregister(STATECHANGE + 'focused') && this._atspiListener.deregister(
+                        													STATECHANGE + 'selected');
     },
 
     deregisterCaretListener : function() {
-        return this._atspiListener.deregister(this._caretMoved);
+        return this._atspiListener.deregister(this.caretMoved);
     },
 
     _notifyUpdate : function(event) {
         let update = null;
 
-        if (event.type.indexOf(this._stateChanged) == 0)
+        if (event.type.indexOf(STATECHANGE) == 0)
             update = 'focus-changed';
-        else if (event.type == this._caretMoved)
+        else if (event.type == caretMoved)
             update = 'caret-moved';
         this.emit(update, event);
     }
 });
 Signals.addSignalMethods(FocusCaretTracker.prototype);
 
-// Should do check for accessible when making call (if necessary)
 function extentsAtROI(caller, event) {
     let extents = [-1 , -1, -1 , -1];
 
-    if (event.type.indexOf('object:state-changed') == 0 && event.detail1 == 1) {
+    if (event.type.indexOf(STATECHANGE) == 0 && event.detail1 == 1) {
         let component = acc.get_component_iface();
         extents = comp.get_extents(Atspi.CoordType.SCREEN);
     }
-    else if (event.type.indexOf('object:text-caret-moved') == 0) {
+    else if (event.type.indexOf(CARETCHANGE) == 0) {
         let text = acc.get_text_iface();
         if (text.get_caret_offset() >= 0)
             extents = text.get_character_extents(text.get_caret_offset(), 0);
